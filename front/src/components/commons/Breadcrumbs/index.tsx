3d06622b77ef2825491092effc58f9f2078ed9f5
Bruna <philippklaus@Anthonys-MBP.fritz.box>
2020-08-29 22:08:21 +0200

    Added ChakraUi breadcrumbs

--- front/src/components/commons/Breadcrumbs/index.tsx
+++ front/src/components/commons/Breadcrumbs/index.tsx
@@ -1,25 +1,29 @@
 import React, { FC, Fragment } from "react";
-import { Item } from "./styles";
-
+import {
+  Breadcrumb as Container,
+  BreadcrumbItem as Item,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from "@chakra-ui/core";
 export interface Props {
   items?: Array<React.ReactNode>;
   separator?: React.ReactNode;
 }
-export const Breadcrumbs: FC<Props> = ({
-  items = [],
-  separator = defaultSeparator,
-}) => {
+export const Breadcrumbs: FC<Props> = ({ items = [], separator }) => {
   const size = items.length;
+
   return (
-    <>
+    <Container>
       {items.map((item, index) => (
         <Fragment key={index}>
-          <Item>{item}</Item>
-          {index < size - 1 && separator}
+          <Item>
+            <BreadcrumbLink href="#">{item}</BreadcrumbLink>
+            {index < size - 1 && (
+              <BreadcrumbSeparator>{separator}</BreadcrumbSeparator>
+            )}
+          </Item>
         </Fragment>
       ))}
-    </>
+    </Container>
   );
 };
-
-export const defaultSeparator = ">";
